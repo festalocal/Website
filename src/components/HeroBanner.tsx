108d830d09220a800c79e7bb4df5d3a69e551468
Haydar SELMI <haydarselmi55@gmail.com>
2023-07-20 13:16:16 +0200

    Correction hero banner

--- src/components/HeroBanner.tsx
+++ src/components/HeroBanner.tsx
@@ -4,9 +4,9 @@ const FindFestivalButton = lazy(() => import("./FindFestivalButton"));
 function HeroBanner() {
   return (
     <>
-      <div className="w-full h-screen">
+      <div className="z-0 w-full h-screen relative -top-16">
         <div className="bg-cover bg-hero-image bg-center w-full h-full fixed"></div>
-        <div className="w-full drop-shadow-lg z-10 backdrop-blur text-white h-screen fixed">
+        <div className="w-full drop-shadow-lg z-0 backdrop-blur text-white h-screen fixed">
           <div className="max-w-lg xl:max-w-2xl px-4 mx-auto flex flex-col justify-center align-center h-full items-center gap-4 my-auto">
             <div>
               <h1 className="font-medium text-4xl lg:text-5xl xl:text-6xl">
